3fa8703d7075c759833a7487b4af2e7924691e6b
pol-33 <123639572+pol-33@users.noreply.github.com>
2025-12-09 12:56:13 +0100

    LineRenderer raygun

--- Assets/RayGun/RayGun.cs
+++ Assets/RayGun/RayGun.cs
@@ -5,6 +5,10 @@ public class RayGun : MonoBehaviour
     // A hand gesture to trigger the ray gun
     public OVRHand leftHand;
     public GameObject leftHandObject;
+    public LineRenderer linePrefab;
+    public Transform shootingPoint;
+    public float maxLineDistance = 5;
+    public float lineShowDuration = 0.3f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -22,5 +26,15 @@ public class RayGun : MonoBehaviour
 
     public void Shoot() {
         Debug.Log("Pew Pew!");
+
+        LineRenderer line = Instantiate(linePrefab);
+        line.positionCount = 2;
+        line.SetPosition(0, shootingPoint.position);
+
+        Vector3 endPosition = shootingPoint.position + shootingPoint.forward * maxLineDistance;
+
+        line.SetPosition(1, endPosition);
+
+        Destroy(line.gameObject, lineShowDuration);
     }
 }
