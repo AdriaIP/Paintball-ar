cc359440572f10114cf34f1eda50823e735a7087
AdriaIP <adria.irigaray@estudiantat.upc.edu>
2025-12-09 15:32:14 +0100

    RayGun rendering target and sound

--- Assets/RayGun/RayGun.cs
+++ Assets/RayGun/RayGun.cs
@@ -2,13 +2,20 @@ using UnityEngine;
 
 public class RayGun : MonoBehaviour
 {
+    public LayerMask hitLayerMask;
     // A hand gesture to trigger the ray gun
     public OVRHand leftHand;
     public GameObject leftHandObject;
     public LineRenderer linePrefab;
+
+    public GameObject rayImpactPrefab;
+
     public Transform shootingPoint;
     public float maxLineDistance = 5;
     public float lineShowDuration = 0.3f;
+
+    public AudioSource audioSource;
+    public AudioClip shootAudioClip;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -25,15 +32,29 @@ public class RayGun : MonoBehaviour
     }
 
     public void Shoot() {
-        Debug.Log("Pew Pew!");
+        audioSource.PlayOneShot(shootAudioClip);
+
+        Ray ray = new Ray(shootingPoint.position, shootingPoint.forward);
+        bool hasHit = Physics.Raycast(ray, out RaycastHit hit, maxLineDistance, hitLayerMask);
+
+        Vector3 endPoint = Vector3.zero;
+
+        if (hasHit) {
+            // stop the ray at the hit point
+            endPoint = hit.point;
+
+            Quaternion rayImpactRotation = Quaternion.LookRotation(hit.normal);
+
+            GameObject rayImpact = Instantiate(rayImpactPrefab, hit.point, rayImpactRotation);
+        } else {
+            endPoint = shootingPoint.position + shootingPoint.forward * maxLineDistance;
+        }
 
         LineRenderer line = Instantiate(linePrefab);
         line.positionCount = 2;
         line.SetPosition(0, shootingPoint.position);
 
-        Vector3 endPosition = shootingPoint.position + shootingPoint.forward * maxLineDistance;
-
-        line.SetPosition(1, endPosition);
+        line.SetPosition(1, endPoint);
 
         Destroy(line.gameObject, lineShowDuration);
     }
